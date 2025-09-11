3e32976d4f3c8da527dce842ff45bf24b3eadaf1
nickosamba <nickosamba.nh21@gmail.com>
2025-09-11 20:52:15 +0100

    creation de l'application :200

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'cloudinary', 
+    'cloudinary_storage'
 ]
 
 MIDDLEWARE = [
