7d7910b579f5cc91048afa1f5f834c861bd21b61
nickosamba <nickosamba.nh21@gmail.com>
2025-11-06 06:54:43 +0100

    build corrigers

--- core/context_processors.py
+++ core/context_processors.py
@@ -8,3 +8,8 @@ def public_user(request):
     except (OperationalError, ProgrammingError):
         user = None
     return {'user': user}
+
+def active_page(request):
+    return {
+        'page': request.resolver_match.url_name
+    }
