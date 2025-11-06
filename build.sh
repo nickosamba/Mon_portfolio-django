d8777eb0c162a00106c74979742602619addf599
nickosamba <nickosamba.nh21@gmail.com>
2025-11-06 05:51:52 +0100

    build corriger

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py createsuperuser --noinput
 
-python manage.py migrate
+
+
