a84d131793b501d218053703d8cb0ad108e0f077
nickosamba <nickosamba.nh21@gmail.com>
2025-11-06 06:49:41 +0100

    build corrigers

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,11 +1,10 @@
 from django.contrib.auth import get_user_model
-
-def active_page(request):
-    return {
-        'page': request.resolver_match.url_name
-    }
+from django.db.utils import OperationalError, ProgrammingError
 
 def public_user(request):
     User = get_user_model()
-    user = User.objects.first()  # ou filtre selon ton logique
+    try:
+        user = User.objects.first()
+    except (OperationalError, ProgrammingError):
+        user = None
     return {'user': user}
